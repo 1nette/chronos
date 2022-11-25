3e6181f3f6ea5ecf623e5947e41517716087aef1
Ihor Khlynin <ihorkhlynin@gmail.com>
2022-11-26 00:40:40 +0200

    ya zaebalsy

---
+++ chronos_backend/controllers/calendar-controller.js
@@ -0,0 +1,39 @@
+const CalendarService = require("../services/calendar-service")
+
+
+class CalendarController {
+    async newCalendar(req, res, next){
+        try{
+            const {title} = req.body
+            const refreshToken = req.cookies.refreshToken
+            const calendar = await CalendarService.newCalendar(title, refreshToken)
+            return res.json(calendar)
+            //const response = async 
+        } catch(e){
+            console.log(e)
+        }
+    }
+    
+    async getCalendar(req, res, next){
+        try{
+            const refreshToken = req.cookies.refreshToken
+            const calendar = await CalendarService.getCalendar(refreshToken)
+            return res.json(calendar)
+        } catch(e) {
+            console.log(e)
+        }
+    }
+
+    async removeCalendar(req, res, next){
+        try{
+            const { id } = req.body
+            const refreshToken = req.cookies.refreshToken
+            const calendarData = await CalendarService.removeCalendar(id, refreshToken)
+            return res.json(calendarData)
+        } catch(e) {
+            console.log(e)
+        }
+    }
+}
+
+module.exports = new CalendarController()
